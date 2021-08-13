68bfd5355273a32e8da9c641e72aa9612217c0b3
wristindex <ajsingh73@gmail.com>
2021-08-12 20:42:16 -0500

    First Commit
    Will be adding more styles to the page

---
+++ scripts/app.js
@@ -0,0 +1,62 @@
+// JS RESPONSIBLE FOR DOM MANIPULATIONS
+
+const cityForm = document.querySelector('form');
+const card = document.querySelector('.card');
+const details = document.querySelector('.detail');
+const time = document.querySelector('img.time');
+const icon = document.querySelector('.icon img');
+
+const updateUI = (data) => {
+
+const {cityDetails, getWeather} = data;
+
+//update details template
+
+details.innerHTML = `
+                <h5 class="my-3">${cityDetails.EnglishName}</h5>
+                <div class="my-3">${getWeather.WeatherText}</div>
+                <div class="display-4 my-4">
+                    <span>${getWeather.Temperature.Metric.Value}</span>
+                    <span>&deg;C</span>
+                </div>
+`;
+
+// update day/night/icon images
+
+const iconSrc = `img/icons/icons/${getWeather.WeatherIcon}.svg`;
+icon.setAttribute('src', iconSrc);
+
+let timeSrc = getWeather.IsDayTime ? 'img/day.svg' : 'img/night.svg';
+    time.setAttribute('src', timeSrc);
+
+
+//remove the d-none class if present
+if (card.classList.contains('d-none')){
+    card.classList.remove('d-none');
+}
+
+};
+
+//referencing the weather data
+
+const updateCity = async (city) => {
+
+    const cityDetails = await getCity(city);
+    const getWeather = await weather(cityDetails.Key);
+
+    return {cityDetails, getWeather};
+
+};
+
+cityForm.addEventListener('submit', e => {
+    e.preventDefault();
+
+    //Get city information
+    const city = cityForm.city.value.trim();
+    cityForm.reset();
+
+    //update ui with new city
+    updateCity(city)
+    .then(data => updateUI(data))
+    .catch(err => console.log(err));
+});
---
+++ scripts/forecast.js
@@ -0,0 +1,40 @@
+// JS RESPONSIBLE FOR INTERACTING WITH THE WEATHER API
+
+const key = 'v9ug0x2bWAvtMFBoF9rDOfiYW5KuILpD';
+
+//retrieving weather information
+const weather = async (ref) => {
+
+    const base = 'http://dataservice.accuweather.com/currentconditions/v1/';
+    const query = `${ref}?apikey=${key}`;
+
+    const response = await fetch(base + query);
+    const data = await response.json();
+
+    return data[0];
+
+};
+
+
+//retrieving city information
+const getCity = async (location) => {
+
+    const base = 'http://dataservice.accuweather.com/locations/v1/cities/search'
+    const query = `?apikey=${key}&q=${location}`;
+
+    const response = await fetch(base + query);
+    const data = await response.json();
+
+    return data[0];
+
+
+};
+
+// Testing async 
+
+// getCity('dallas').then(data => {
+//         return weather(data.Key);  //this is a promise where we enter the Key received into weather
+//     }).then(data => {
+//         console.log(data);
+//     }).catch(err => console.log(err));
+
